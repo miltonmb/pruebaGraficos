9cf878f120dd822a454e195a21a126395a74dd0e
miltonmb <miltonmb@unitec.edu>
2018-07-10 19:55:31 -0600

    cambios completos

--- test.js
+++ test.js
@@ -1,39 +1,36 @@
 window.onload = function () {
-
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        theme: "light2", // "light1", "light2", "dark1", "dark2"
-        title:{
-            text: "Sales By Month for"
-        },
-        axisY: {
-            title: "Reserves(MMbbl)"
-        },
-        data: [{        
-            type: "column",  
-            showInLegend: true, 
-            legendMarkerColor: "rgb(114, 168, 255)",
-            legendText: "Ventas",
-            color: "rgb(114, 168, 255)",
-            dataPoints: [      
-                { y: 100, label: "January" },
-                { y: 490,  label: "February" },
-                { y: 400,  label: "March" },
-                { y: 520,  label: "April" }
-            ]
-        }]
-    });
-    chart.render();
-    
-    }
-
+  var chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    theme: 'light2', // "light1", "light2", "dark1", "dark2"
+    title: {
+      text: 'Sales By Month for'
+    },
+    axisY: {
+      title: 'Ventas'
+    },
+    data: [{
+      type: 'column',
+      showInLegend: true,
+      legendMarkerColor: 'rgb(114, 168, 255)',
+      legendText: 'Ventas',
+      color: 'rgb(114, 168, 255)',
+      dataPoints: [
+        { y: 100, label: 'January' },
+        { y: 490,  label: 'February' },
+        { y: 400,  label: 'March' },
+        { y: 520,  label: 'April' }
+      ]
+    }]
+  })
+  chart.render()
+}
 
 function onSelectChange (x) {
   if (x == 'comida') {
     var newOptions = {'Frutas': 'frutas',
       'Cereales': 'cereales'
     }
-
+    barUpdate(200, 250, 380, 430)
     var $el = $('#prod')
     $el.empty()
     $.each(newOptions, function (key, value) {
@@ -55,7 +52,7 @@ function onSelectChange (x) {
     var newOptions = {'Camisa': 'camisa',
       'Pantalon': 'pantalon'
     }
-
+    barUpdate(100, 490, 400, 520)
     var $el = $('#prod')
     $el.empty()
     $.each(newOptions, function (key, value) {
@@ -81,7 +78,7 @@ function onSelectChangeProd (x) {
     var newOptions = {'Pantalon1': 'pantalon1',
       'Pantalon2': 'pantalon2'
     }
-
+    barUpdate(230, 240, 380, 535)
     var $el = $('#marca')
     $el.empty()
     $.each(newOptions, function (key, value) {
@@ -92,7 +89,7 @@ function onSelectChangeProd (x) {
     var newOptions = {'Camisa1': 'camisa1',
       'Camisa2': 'camisa2'
     }
-
+    barUpdate(100, 490, 400, 520)
     var $el = $('#marca')
     $el.empty()
     $.each(newOptions, function (key, value) {
@@ -103,7 +100,7 @@ function onSelectChangeProd (x) {
     var newOptions = {'Frutas1': 'frutas1',
       'Frutas2': 'frutas2'
     }
-
+    barUpdate(200, 250, 380, 430)
     var $el = $('#marca')
     $el.empty()
     $.each(newOptions, function (key, value) {
@@ -114,7 +111,7 @@ function onSelectChangeProd (x) {
     var newOptions = {'Cereales1': 'cereales1',
       'Cereales2': 'cereales2'
     }
-
+    barUpdate(260, 345, 475, 580)
     var $el = $('#marca')
     $el.empty()
     $.each(newOptions, function (key, value) {
@@ -124,6 +121,49 @@ function onSelectChangeProd (x) {
   }
 }
 
+function onSelectChangeMarca (x) {
+  if (x == 'camisas1') {
+    barUpdate(100, 490, 400, 520)
+  } else if (x == 'camisas2') {
+    barUpdate(120, 495, 410, 530)
+  } else if (x == 'pantalon1') {
+    barUpdate(230, 240, 380, 535)
+  } else if (x == 'pantalon2') {
+    barUpdate(235, 230, 390, 545)
+  } else if (x == 'frutas1') {
+    barUpdate(200, 250, 380, 430)
+  } else if (x == 'frutas2') {
+    barUpdate(210, 270, 360, 420)
+  } else if (x == 'cereales1') {
+    barUpdate(260, 345, 475, 580)
+  } else if (x == 'cereales2') {
+    barUpdate(280, 335, 475, 570)
+  }
+}
+
 function barUpdate (v1, v2, v3, v4) {
-    
+  var chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    theme: 'light2', // "light1", "light2", "dark1", "dark2"
+    title: {
+      text: 'Sales By Month for'
+    },
+    axisY: {
+      title: 'Ventas'
+    },
+    data: [{
+      type: 'column',
+      showInLegend: true,
+      legendMarkerColor: 'rgb(114, 168, 255)',
+      legendText: 'Ventas',
+      color: 'rgb(114, 168, 255)',
+      dataPoints: [
+        { y: v1, label: 'January' },
+        { y: v2,  label: 'February' },
+        { y: v3,  label: 'March' },
+        { y: v4,  label: 'April' }
+      ]
+    }]
+  })
+  chart.render()
 }
